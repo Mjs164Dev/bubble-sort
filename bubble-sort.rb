07bf6289c424a4598013c92474952baf7def83c1
Mike Schmid <152652501+Mjs164Dev@users.noreply.github.com>
2024-02-09 14:50:40 -0500

    loop through with a for loop.

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -1,7 +1,26 @@
-unsorted_array = [4,3,78,2,0,2]
+unsorted_array = [4,3,78,2,0,2,17,4,24,2]
 
 def bubble_sort(unsorted_array)
-  
+  for i in 0..unsorted_array.length - 2
+    first_num = unsorted_array[i]
+    second_num = unsorted_array[i + 1]
+
+    puts "Comparison: (#{first_num} : #{second_num})"
+
+    #if first_num > second_num
+      #first_num = unsorted_array.delete_at(i)
+      #unsorted_array.insert(first_num, i + 1)
+      #puts "Numbers swapped!"
+      #puts "Updated array: #{unsorted_array}"
+    #end
+  end
 end
 
 bubble_sort(unsorted_array)
+
+
+
+# first_num = unsorted_array.delete_at(index)
+# second_num = unsorted_array.delete_at(index + 1)
+# unsorted_array.insert(index + 1,first_num)
+# unsorted_array.insert(index, second_num)
